d4d7ee6af54720d0e3e3b816cd7facfe274486e8
Erik Eguskiza Aranda <119065295+eeguskiza@users.noreply.github.com>
2023-05-02 22:30:13 +0200

    Add files via upload

---
+++ Java/ClubDeMontaña/src/MisCojones33/Main.java
@@ -0,0 +1,14 @@
+package MisCojones33;
+
+public class Main {
+
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				Decathlon app = new Decathlon();
+			}
+		});
+
+	}
+
+}
---
+++ Java/ClubDeMontaña/src/MisCojones33/Socio.java
@@ -0,0 +1,57 @@
+package MisCojones33;
+
+public class Socio {
+	//Atributos Socio
+		private String nombre; 
+		private String dni; 
+		private String email; 
+		private String telefono;
+		
+		public Socio(String nombre, String dni, String email, String telefono) {
+			super();
+			this.nombre = nombre;
+			this.dni = dni;
+			this.email = email;
+			this.telefono = telefono;
+		}
+
+		public String getNombre() {
+			return nombre;
+		}
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+		public String getDni() {
+			return dni;
+		}
+
+		public void setDni(String dni) {
+			this.dni = dni;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public String getTelefono() {
+			return telefono;
+		}
+
+		public void setTelefono(String telefono) {
+			this.telefono = telefono;
+		}
+
+		@Override
+		public String toString() {
+			return "Socio [nombre=" + nombre + ", dni=" + dni + ", email=" + email + ", telefono=" + telefono + "]";
+		}
+		
+		
+
+}
